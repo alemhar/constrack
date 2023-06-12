586f9f56a4c6f90202546dda185aee0aa24c2838
Alemhar <alemhar@gmail.com>
2023-06-12 21:43:05 +0800

    Initial Config.

--- resources/js/Pages/Welcome.vue
+++
@@ -1,3 +0,0 @@
-<template>
-    <div><h1>Hellow World</h1></div>
-</template>
--- resources/js/app.js
+++ resources/js/app.js
@@ -1,8 +1,20 @@
 import { createApp, h } from 'vue'
 import { createInertiaApp } from '@inertiajs/vue3'
+// import Layout from "./Shared/Layout"
 
 createInertiaApp({
   resolve: name => require(`./Pages/${name}`),
+
+  // resolve: name => {
+  //   let page = require(`./Pages/${name}`).default;
+  //   page.layout ??=Layout;
+  // },
+
+  // resolve: async name => {
+  //   let page = (await import(`./Pages/${name}`)).default;
+  //   page.layout ??=Layout;
+  // },
+
   setup({ el, App, props, plugin }) {
     createApp({ render: () => h(App, props) })
       .use(plugin)
